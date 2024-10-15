6a10f7f508d0de8752e9adde602e51bd465add10
Shahan-07 <iamshahanalam22@gmail.com>
2024-10-15 12:31:18 +0500

    While loop in js

---
+++ 05-iteration.js/whileloop.js
@@ -0,0 +1,28 @@
+
+    // while loop structure
+
+let index = 0;
+while (index <= 10) {
+    console.log(`value of index is : ${index}`);
+    index = index + 2;
+}
+
+    // Arrays in while loop
+
+let myArray = ["Batman","Superman","ironman"];
+
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(`valu is ${myArray[arr]}`);
+    arr++
+}
+
+
+    // Do while loop
+
+let score = 1;
+
+do {
+    console.log(`score is : ${score}`);
+    score++;
+} while (score <=10);
