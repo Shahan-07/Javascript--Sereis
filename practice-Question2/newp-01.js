c939ad9bacdf4a358b5d6ffc283d365459a16b2a
Shahan-07 <iamshahanalam22@gmail.com>
2024-11-02 13:27:50 +0500

    practicing on javascript part 2

--- practice-Question2/newp-01.js
+++ practice-Question2/newp-01.js
@@ -79,32 +79,32 @@
             //console.log("Random Number : ", randomNumber);     // gives random number between 0 and 1
 
             const randomInt = Math.floor(Math.random() * 10) +1;
-            console.log(randomInt);
+            //console.log(randomInt);
 
 
 
                      // number is even or odd
 
-                prompt('please enter the number');
-                if(num % 2 === 0){
-                    console.log('number is even')
-                }else{
-                    console.log('number is odd');
-                }
+                // prompt('please enter the number');
+                // if(num % 2 === 0){
+                //     console.log('number is even')
+                // }else{
+                //     console.log('number is odd');
+                // }
             
             
 
                 // JavaScript Program to Check if a Number is a Prime Number
 
-            const number = parseInt(prompt('Enter a number'));
-            let prime  = number > 1;
+            // const number = parseInt(prompt('Enter a number'));
+            // let prime  = number > 1;
 
-            for(let i =2; i < number; i++){
-                if(number % 1 === 0){
-                    prime = false;
-                    break;
-                }
-            }
+            // for(let i =2; i < number; i++){
+            //     if(number % 1 === 0){
+            //         prime = false;
+            //         break;
+            //     }
+            // }
 
          //console.log(prime ?`${number} is a prime number`:`${number} is not a prime number`);
             
@@ -123,13 +123,109 @@
             }
 
 
-            let a = prompt('please enter the first number')
-            let b = prompt('please enter the second number')
-            let c = prompt('please enter the third number')
+            // let a = prompt('please enter the first number')
+            // let b = prompt('please enter the second number')
+            // let c = prompt('please enter the third number')
 
-            let result = check(a,b,c);
-            console.log(`${a},${b},${c} : Largest = ${result}`);
+            // let result = check(a,b,c);
+            //console.log(`${a},${b},${c} : Largest = ${result}`);
 
 
+
+                    //Program to Find the Factorial of a Number
+
+            // let num = prompt('please enter a number');
+            // let fact = 1;
+
+            // if(num == 0){
+            //     console.log(`The factorial of ${num} is 1`);
+            // }else if(num < 0){
+            //    // console.log(`The factorial of -ive number is not possible`);
+            // }else{
+            //     for(let i = 1; i<=num; i++){
+            //         fact = fact * i;
+            //     }
+            //     //console.log(`The factorail of ${num} is ${fact}`);
+            // }
+
+
+                    // Program to print the table of any user
+
+            // function table (number){
+            //     for(let i =1; i <= 10;i++){
+            //         let result = i * number;
+            //         console.log(`${number} x ${i} = ${result}`);
+            //     }
+            // }
+            // table(5);
+
+
+
+                // simple Program to make a simple calculator using switch case in JavaScript
+
+            // function calculator(num1,num2,operator){
+            //     let  result;
+            //     switch(operator){
+            //         case'+':
+            //             result = num1 + num2;
+            //             break;
+
+            //         case '-':
+            //             result = num1 - num2;
+            //             break;
+
+            //         case'*':
+            //             result = num1 * num2;
+            //             break;
+
+            //         case'/':
+            //             result = num1 / num2;
+            //             break;
+                    
+            //         default: 
+            //             result = 'invalid operator'
+            //     }
+            //     return result;
+            // }
+
+            // let num1 = parseInt(prompt("Enter the first number"));
+            // let num2 = parseInt(prompt("Enter the second number"));
+            // let operator = prompt("Enter the operator (+, -, *, /): ");
+
+            // let output = calculator(num1,num2,operator);
+            // console.log("result :",output);
             
             
+
+                    // Program to Check if the Numbers Have Same Last Digit
+
+
+            // let a = prompt("please enter 'a' number");
+            // let b = prompt("please enter 'b' number");
+            // let c = prompt("please enter 'c' number");
+
+
+            // let num1 = a % 10;
+            // let num2 = b % 10;
+            // let num3 = c % 10;
+
+            // if(num1 === num2 && num1 === num3){
+            //     console.log(`${a},${b},${c} are having the same last digit`);
+            // } else{
+            //     console.log(`${a},${b},${c} are not having the same last digit`);
+            // }
+            
+            
+
+
+                        // Program to Print the Fibonacci sequence
+
+                let a=0;
+                let b= 1;
+                    for(let i = 0;i <=5; i++){
+                        let temp = a + b;
+                        a = b;
+                        b = temp;
+                        console.log(temp);
+                        
+                    }
