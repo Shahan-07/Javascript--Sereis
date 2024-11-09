8da9a8fa79bcec6b1e7f5ba6892c17d482305589
Shahan-07 <iamshahanalam22@gmail.com>
2024-11-09 14:13:08 +0500

    DOM exercise 4

--- 06-dom/four.js
+++ 06-dom/four.js
@@ -8,7 +8,34 @@ function addLanguage(langName){
     li.appendChild(addText);
     document.querySelector('.language').appendChild(li);
 }
-addLanguage('java');
+addLanguage('Rust');
+
+
+function addinglanguage(language){
+    const li = document.createElement('li');
+    const textAdding = document.createTextNode(language);
+    li.appendChild(textAdding);
+    document.querySelector('.language').appendChild(li);
+}
+addinglanguage('GoLang');
+
+
+
+function newLanguage(newlang){
+    const li = document.createElement('li');
+    const text = document.createTextNode(newlang);
+    li.appendChild(text);
+    document.querySelector('.language').appendChild(li);
+}
+newLanguage('Javascript');
+
+function addPara(para){
+    const newElemnt = document.createElement('p');
+    const addtext = document.createTextNode(para);
+    newElemnt.appendChild(addtext);
+    document.querySelector('.paragraph').appendChild(newElemnt);
+}
+addPara('this is new paragraph');
 
 
 function add(lang){
@@ -21,21 +48,28 @@ add('Hello');
 
 
 
+
+
         // editing the elements
 
-    const secondchild = document.querySelector('li:nth-child(2)');
-    const newli = document.createElement('li');
-    newli.textContent = 'Go'
-    secondchild.replaceWith(newli);
+    // const secondchild = document.querySelector('li:nth-child(2)');
+    // const newli = document.createElement('li');
+    // newli.textContent = 'Go'
+    // secondchild.replaceWith(newli);
 
-    const firstchild = document.querySelector('li:first-child');
-    const newl= document.createElement('li');
-    newl.textContent = "python";
-    firstchild.replaceWith(newl);
+    // const first = document.querySelector('li:first-child');
+    // const neew = document.createElement('li');
+    // neew.textContent = "AI";
+    // first.replaceWith(neew);
 
+    const thirdChild = document.querySelector('li:nth-child(3)');
+    const newElement = document.createElement('li');
+    newElement.textContent = 'new Language';
+    thirdChild.replaceWith(newElement);
 
 
             // Remove an element
 
-        const lastlang = document.querySelector('li:last-child');
-        lastlang.remove();
+        // const lastlang = document.querySelector('li:last-child');
+        // lastlang.remove();
+
