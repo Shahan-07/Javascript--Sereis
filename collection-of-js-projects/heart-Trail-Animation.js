d97444847e31a4c027dc6ce002b30a67a79094ab
Shahan-07 <iamshahanalam22@gmail.com>
2024-11-15 23:54:40 +0500

    Heart-Trail-Animation project

---
+++ collection-of-js-projects/heart-Trail-Animation.js
@@ -0,0 +1,8 @@
+const bodyElement = document.querySelector('body');
+
+bodyElement.addEventListener('mousemove',(event)=>{
+    const xPosition = event.offsetX;
+    const yPosition = event.offsetY;
+    const spanElement = document.createElement('span');
+    
+})
