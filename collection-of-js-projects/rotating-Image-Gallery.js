7b7b60ae4eac46c44b877aa54bcf914404000ef1
Shahan-07 <iamshahanalam22@gmail.com>
2024-11-22 22:30:16 +0500

    Rotating-Image-Gallery project of JS

---
+++ collection-of-js-projects/rotating-Image-Gallery.js
@@ -0,0 +1,21 @@
+const imageEl = document.querySelector(".image-container");
+
+const prevEl = document.getElementById("prev");
+const nextEl = document.getElementById("next");
+
+let x = 0;
+
+prevEl.addEventListener("click",()=>{
+    x = x + 45;
+    updateGallery();
+});
+nextEl.addEventListener("click",()=>{
+    x = x - 45;
+    updateGallery();
+});
+
+function updateGallery(){
+    imageEl.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+}
+
+
