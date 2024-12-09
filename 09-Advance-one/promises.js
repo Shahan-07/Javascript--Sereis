186c0c37b2693c2cd8324dcb27c93b6ed2cceded
Shahan-07 <iamshahanalam22@gmail.com>
2024-12-09 18:53:38 +0500

    Promises in JS

--- 09-Advance-one/promises.js
+++ 09-Advance-one/promises.js
@@ -78,10 +78,34 @@ const promiseFive = new Promise((resolve, reject) => {
 async function consumePromsieFive() {
     try {
         const response = await promiseFive
-        console.log(response);
+      //  console.log(response);
     } catch (error) {
         console.log(error);
         
     }
 }
 consumePromsieFive();
+
+
+async function getAllUser() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        //console.log(data);
+    } catch (error) {                                       // handling the data using try catch method
+        //console.log("Errors");
+        
+    }
+}
+getAllUser();
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{console.log(error);
+});
