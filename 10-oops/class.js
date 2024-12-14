d5bb5a1772f0f3e07846596caa7bbff6b642740c
Shahan-07 <iamshahanalam22@gmail.com>
2024-12-14 14:43:16 +0500

    classes in JS

---
+++ 10-oops/class.js
@@ -0,0 +1,52 @@
+// class user{
+
+
+//     constructor(username,email,password){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
+
+//     encryptPassword(){
+//         return `${this.password}abc `
+//     }
+
+//     changeUserName(){
+//         return `${this.username.toUpperCase()}`
+//     }
+
+//     changeGmail(){
+//         return `${this.email.toUpperCase()}`
+//     }
+
+// };
+
+// const chai = new user("chai","chai@gmail",12345);
+// console.log(chai.encryptPassword());
+// console.log(chai.changeUserName());
+// console.log(chai.changeGmail());
+
+
+                    // Behind the Scene when Class was not present on Early stages //
+
+            
+            function user(username,email,password){
+                this.username = username;
+                this.email = email;
+                this.password = password;
+            }
+
+            user.prototype.encryptPassword = function(){
+                return `${this.password}abcd`;
+            }
+
+            user.prototype.changeGmail = function(){
+                return `${this.email.toUpperCase()}`
+            }
+
+            const tea = new user("tea","tea@gmail",1232123);
+
+            console.log(tea.encryptPassword());
+            console.log(tea.changeGmail());
+            
+            
