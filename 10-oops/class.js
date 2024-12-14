af951e3f96d567a6eca0e67f3121e8eecc53417d
Shahan-07 <iamshahanalam22@gmail.com>
2024-12-14 14:48:43 +0500

    Classes in JS

--- 10-oops/class.js
+++ 10-oops/class.js
@@ -1,30 +1,30 @@
-// class user{
+    class user{
 
 
-//     constructor(username,email,password){
-//         this.username = username;
-//         this.email = email;
-//         this.password = password;
-//     }
+        constructor(username,email,password){
+            this.username = username;
+            this.email = email;
+            this.password = password;
+        }
 
-//     encryptPassword(){
-//         return `${this.password}abc `
-//     }
+        encryptPassword(){
+            return `${this.password}abc `
+        }
 
-//     changeUserName(){
-//         return `${this.username.toUpperCase()}`
-//     }
+        changeUserName(){
+            return `${this.username.toUpperCase()}`
+        }
 
-//     changeGmail(){
-//         return `${this.email.toUpperCase()}`
-//     }
+        changeGmail(){
+            return `${this.email.toUpperCase()}`
+        }
 
-// };
+    };
 
-// const chai = new user("chai","chai@gmail",12345);
-// console.log(chai.encryptPassword());
-// console.log(chai.changeUserName());
-// console.log(chai.changeGmail());
+    const chai = new user("chai","chai@gmail",12345);
+    console.log(chai.encryptPassword());
+    console.log(chai.changeUserName());
+    console.log(chai.changeGmail());
 
 
                     // Behind the Scene when Class was not present on Early stages //
