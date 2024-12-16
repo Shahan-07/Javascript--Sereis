67da45dcec9800a780cedf319f3ae48d101ea914
Shahan-07 <iamshahanalam22@gmail.com>
2024-12-16 15:01:21 +0500

    Getter and Setter in JS

---
+++ 10-oops/getter-setter.js
@@ -0,0 +1,23 @@
+class user{
+    constructor(email,password){
+        this.email = email;
+        this.password = password;               
+    }
+
+    get password(){
+        return this._password.toUpperCase();
+    }
+    set password(value){
+        this._password = value.toUpperCase();
+    }
+    get email(){
+        return this._email.toUpperCase();
+    }
+    set email(value){
+        this._email = value.toUpperCase();
+    }
+}
+
+const chai  = new user("chai@gmail.com", "abcds");
+console.log(chai._password);
+console.log(chai._email);
