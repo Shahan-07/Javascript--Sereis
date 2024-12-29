948b4269088a7e456c34820112be857c3243e65d
Shahan-07 <iamshahanalam22@gmail.com>
2024-12-29 15:26:56 +0500

    practice project 1

---
+++ JS-projects/practice-project-1/app.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+const name = prompt("whats your name? ");
+console.log("Hello", name, "Welcome to Our Game!");
+
+const shouldWePlay = prompt("Do you want to play? (yes/no): ");
