1409ef7919e90366b2aa3d982762339934ad1eb4
Shahan-07 <iamshahanalam22@gmail.com>
2024-12-30 15:38:07 +0500

    Mini-Adventure-Game

--- JS-projects/practice-project-1/app.js
+++ JS-projects/practice-project-1/app.js
@@ -4,7 +4,27 @@ console.log("Hello", name, "Welcome to Our Game!");
 
 const shouldWePlay = prompt("Do you want to play? ");
 
-const condition = shouldWePlay.toLowerCase() === "yes";
-console.log(condition);
+if(shouldWePlay.toLowerCase() === "yes"){
+    // Game Logic
+    const leftOrRight = prompt("You Enter a Maze, do you want to go left or right? ");
+    if(leftOrRight === "left"){
+        console.log("you go left and see a bridge");
+        const cross = prompt("Do you want to cross the bridge? ").toLowerCase();
 
+        if(cross === "yes" || cross === "y" || cross === "okay"){
+            console.log("you crossed but the bridge was weak and broke and you fell. you losstt...");
+        } else{
+            console.log("Good choice you win");
+        }
+    } else{
+        console.log("you go right and fall off a cliff...");
+        
+    }
+
+
+} else if(shouldWePlay.toLowerCase() === "no"){
+    console.log("Okay :(");
+} else{
+    console.log("Invalid input");
+}
 
