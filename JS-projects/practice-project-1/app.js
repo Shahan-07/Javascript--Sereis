21b59db3716f9544fc17495aa674bcc3a8a9a389
Shahan-07 <iamshahanalam22@gmail.com>
2024-12-30 12:39:01 +0500

    practice projects of JS

--- JS-projects/practice-project-1/app.js
+++ JS-projects/practice-project-1/app.js
@@ -2,4 +2,9 @@ const prompt = require("prompt-sync")();
 const name = prompt("whats your name? ");
 console.log("Hello", name, "Welcome to Our Game!");
 
-const shouldWePlay = prompt("Do you want to play? (yes/no): ");
+const shouldWePlay = prompt("Do you want to play? ");
+
+const condition = shouldWePlay.toLowerCase() === "yes";
+console.log(condition);
+
+
